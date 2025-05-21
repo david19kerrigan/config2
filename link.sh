5025257288ec1c86dfc313cc80d07f49dd4a6025
david19kerrigan <david19kerrigan@gmail.com>
2025-05-20 21:32:51 -0400

    add tmuxZ

--- link.sh
+++ link.sh
@@ -4,4 +4,5 @@ ln -sf $PWD/.vim ~/
 ln -sf $PWD/.tmux.conf ~/.tmux.conf
 ln -sf $PWD/alacritty ~/.config/
 ln -sf $PWD/.emacs ~/.emacs
+ln -sf $PWD/.tmux.conf ~/
 ln -sf $PWD/colorschemes ~/.local/share/xfce4/terminal/
